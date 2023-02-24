d70849d3a602704d822caee688e3883aae00821c
Kurizu <83665497+crizmo@users.noreply.github.com>
2023-02-24 04:50:07 +0000

    fix

--- practs/flutter/helloworld.dart
+++ practs/flutter/helloworld.dart
@@ -4,64 +4,38 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // Application name
-
-      title: 'Flutter Stateful Clicker Counter',
-
+      title: 'Flutter Hello World',
+      // Application theme data, you can set the colors for the application as
+      // you want
       theme: ThemeData(
-        // Application theme data, you can set the colors for the application as
-
-        // you want
-
-        primarySwatch: Colors.orange,
+        primarySwatch: Colors.blue,
       ),
-
-      home: MyHomePage(),
+      // A widget which will be started on application startup
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Social')),
+      appBar: AppBar(
+        // The title text which will be shown on the action bar
+        title: Text(title),
+      ),
       body: Center(
         child: Text(
-          "Hello There",
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 40.0,
-          ),
+          'Hello, World!',
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-          elevation: 10.0,
-          child: Icon(Icons.people_alt_rounded),
-          onPressed: () {
-            // action on button press
-          }),
-      bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 0,
-          fixedColor: Colors.deepOrange,
-          items: [
-            BottomNavigationBarItem(
-              label: ("Home"),
-              icon: Icon(Icons.home),
-            ),
-            BottomNavigationBarItem(
-              label: ("Search"),
-              icon: Icon(Icons.search),
-            ),
-            BottomNavigationBarItem(
-              label: ("Profile"),
-              icon: Icon(Icons.account_circle),
-            ),
-          ],
-          onTap: (int indexOfItem) {}),
     );
   }
 }
