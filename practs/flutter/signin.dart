8f403f0cac815dc99f7b152a62e1ee0cf3d67c6d
Kurizu <83665497+crizmo@users.noreply.github.com>
2023-02-26 12:32:10 +0000

    signin

---
+++ practs/flutter/signin.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+// This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+// Application name
+
+      title: 'Flutter Stateful Clicker Counter',
+
+      theme: ThemeData(
+// Application theme data, you can set the colors for the application as
+
+// you want
+
+        primarySwatch: Colors.blue,
+      ),
+
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Hello World')),
+        body: Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(15),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'User Name',
+                      hintText: 'Enter Your Name',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(15),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      hintText: 'Enter Password',
+                    ),
+                  ),
+                ),
+                RaisedButton(
+                  textColor: Colors.white,
+                  color: Colors.blue,
+                  child: Text('Sign In'),
+                  onPressed: () {},
+                )
+              ],
+            )),
+        floatingActionButton: FloatingActionButton(
+            elevation: 10.0, child: Icon(Icons.add), onPressed: () {}));
+  }
+}
